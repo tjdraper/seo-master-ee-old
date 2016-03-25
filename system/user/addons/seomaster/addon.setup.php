953e9e7debf30035d53a456c6d9d22cf95a9f7e9
TJ Draper <tj@buzzingpixel.com>
2016-03-24 23:39:14 -0500

    Refactor directory structure for EE 3

--- system/user/addons/seomaster/addon.setup.php
+++ system/user/addons/seomaster/addon.setup.php
@@ -8,29 +8,6 @@ defined('SEOMASTER_NAME') || define('SEOMASTER_NAME', 'SEO Master');
 defined('SEOMASTER_NAMESPACE') || define('SEOMASTER_NAMESPACE', 'BuzzingPixel\SeoMaster');
 defined('SEOMASTER_VER') || define('SEOMASTER_VER', '1.0.0');
 
-// Set up autoloading
-spl_autoload_register(function ($class) {
-	$ns = explode('\\', $class);
-
-	// Make sure this request is for us
-	if (count($ns) > 2 && "{$ns[0]}\\{$ns[1]}" !== SEOMASTER_NAMESPACE) {
-		return;
-	}
-
-	// Remove start of namespace
-	unset($ns[0], $ns[1]);
-
-	// Put the file path together
-	$ns = implode(DIRECTORY_SEPARATOR, $ns);
-
-	// Load the file
-	$file = PATH_THIRD . 'seomaster' . DIRECTORY_SEPARATOR . $ns . '.php';
-
-	if (file_exists($file)) {
-		include_once $file;
-	}
-});
-
 return array(
 	'author' => SEOMASTER_AUTHOR,
 	'author_url' => SEOMASTER_AUTHOR_URL,
